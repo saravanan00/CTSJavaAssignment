1b4114dadb223a4cf5441beb41afd5e32b04e843
saravanan00 <saravananms1296@gmail.com>
2018-05-23 14:46:24 +0530

    use meaningfull variable names

--- src/com/cts/arraylist/ArrayListInteger.java
+++ src/com/cts/arraylist/ArrayListInteger.java
@@ -6,6 +6,7 @@ import java.util.List;
 
 public class ArrayListInteger {
 	public boolean integerId(List<Integer> list, int a) {
+		//what is a and what is ss?
 		for (Integer ss : list) {
 			if (ss == a) {
 				return true;
--- src/com/cts/servicetest/EmployeeDetailsTest.java
+++ src/com/cts/servicetest/EmployeeDetailsTest.java
@@ -1,5 +1,12 @@
 package com.cts.servicetest;
 
+import com.cts.serviceimpl.EmployeeDetailsServiceImpl;
+
 public class EmployeeDetailsTest {
 
+	public static void main(String[]args)
+	{
+		EmployeeDetailsServiceImpl employeedetails=new EmployeeDetailsServiceImpl();
+		
+	}
 }
