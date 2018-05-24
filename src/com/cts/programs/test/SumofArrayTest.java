52788956697c46b03c83865a35504c0caadf4602
saravanan00 <saravananms1296@gmail.com>
2018-05-24 10:41:12 +0530

    modified basics

--- src/com/cts/programs/test/SumofArrayTest.java
+++ src/com/cts/programs/test/SumofArrayTest.java
@@ -2,11 +2,11 @@ package com.cts.programs.test;
 
 import com.cts.programs.ArraySum;
 
-public class SumOfArrayTest {public class SumofArrayTest {
-	public static void main (String[]args)
+public class SumofArrayTest {
+	public static void main (String[] args)
 	{
 	ArraySum obj=new ArraySum();
 		int[] a={10,10,10,10};
 		System.out.println(obj.sumArray(a));
 	}
-	}}
+	}
